a6478d41d23bc42967e68c5ca40b6d6e8f5be4f2
AMereBagatelle <themerebagatelle@gmail.com>
2020-09-17 14:22:32 -0400

    Create ConfigScreen base

---
+++ src/main/java/amerebagatelle/github/io/fabricskyboxes/gui/ConfigScreen.java
@@ -0,0 +1,23 @@
+package amerebagatelle.github.io.fabricskyboxes.gui;
+
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.text.Text;
+
+public class ConfigScreen extends Screen {
+    public ConfigScreen(Text title) {
+        super(title);
+    }
+
+    @Override
+    public void init(MinecraftClient client, int width, int height) {
+        super.init(client, width, height);
+    }
+
+    @Override
+    public void render(MatrixStack matrices, int mouseX, int mouseY, float delta) {
+        super.render(matrices, mouseX, mouseY, delta);
+    }
+
+}
